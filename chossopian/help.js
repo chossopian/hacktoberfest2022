4931ccb5833c67312f4dae3bc8ce5fe319efbcd2
chossopian <chos.sopian@gmail.com>
2023-10-28 08:05:32 +0700

    edit help.js 2

--- chossopian/help.js
+++ chossopian/help.js
@@ -2,3 +2,4 @@ const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
 pptr.use(AdblockerPlugin({blockTrackers: true}));
 const delay = require('delay');
 const chromePaths = require('chrome-paths');
+const fetch = require('node-fetch');
